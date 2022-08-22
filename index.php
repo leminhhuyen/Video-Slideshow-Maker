2565b1be061d5fa3829c13aa22be44c3fb08f4f5
HUYEN <leminhhuyen1999130@gmail.com>
2022-08-23 02:58:11 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Video Slideshow Maker</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body class="hold-transition login-page" style="
+    background-image: url('./1.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;">
+
+</body>
+</html>
